84b255e50c1871a29e7c4bf4cdcc8d67905b9734
Martin Wong <martinytwong2020@gmail.com>
2022-10-01 23:16:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { hydrateRoot } from 'react-dom/client';
 import './App.css';
-
 function App() {
+  let result="";
+  const [url,SetUrl]=useState("");
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/cors', {mode:'cors'});
+      const data = await response.json();
+      console.log(data["http:"])
+      SetUrl(data["http:"]);
+      return data;
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    result= makeAPICall()
+    .then(function(result){
+      // console.log(result["http:"]);
+    })
+    
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Cors Guide</h1>
+      <h2>{url}</h2>
+      <img className="img_" src ={url}/>
     </div>
   );
 }
-
 export default App;
